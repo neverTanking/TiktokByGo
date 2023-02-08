3a3dce94a2472d5e4507aea8d1f0d58896534f3e
孤灯灵 <2809551464@qq.com>
2023-02-08 22:49:39 +0800

    🎈 perf: 增加 msg 文件，替换魔法数值

---
+++ controller/msg.go
@@ -0,0 +1,13 @@
+package controller
+
+const (
+	// StatusCode
+	Success = 0
+	Fail    = 1
+
+	NotExisted    = "User not exist" // 用户不存在
+	WrongPassword = "Wrong password" // 登录密码错误
+	SignUpOk      = "Sign up ok"     // 注册成功
+	LoginOk       = "Login ok"       // 登录成功
+	NotLogin      = "User not login" // 用户未登录
+)
