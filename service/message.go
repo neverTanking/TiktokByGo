689882318cde5203616e2e43eab8d85ad931dbfc
孤灯灵 <2809551464@qq.com>
2023-02-08 23:26:58 +0800

    🔧 build: 修改module文件

--- service/message.go
+++ service/message.go
@@ -3,10 +3,11 @@ package service
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/RaymondCode/simple-demo/controller"
 	"io"
 	"net"
 	"sync"
+
+	"github.com/neverTanking/TiktokByGo/controller"
 )
 
 var chatConnMap = sync.Map{}
