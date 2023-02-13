1247b2732001104e363a24ced8dda996ad92a6fb
孤灯灵 <2809551464@qq.com>
2023-02-13 15:24:34 +0800

    🎈 perf: user功能简单实现

--- controller/user.go
+++ controller/user.go
@@ -1,22 +1,41 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+	"strings"
 	"sync/atomic"
+
+	"github.com/gin-gonic/gin"
 )
 
 // usersLoginInfo use map to store user info, and key is username+password for demo
 // user data will be cleared every time the server starts
 // test data: username=zhanglei, password=douyin
-var usersLoginInfo = map[string]User{
-	"zhangleidouyin": {
+// var usersLoginInfo = map[string]User{
+// 	"zhangleidouyin": {
+// 		Id:            1,
+// 		Name:          "zhanglei",
+// 		FollowCount:   10,
+// 		FollowerCount: 5,
+// 		IsFollow:      true,
+// 	},
+// }
+
+// userId -> user
+var usersDate = map[int64]User{
+	1: {
 		Id:            1,
 		Name:          "zhanglei",
 		FollowCount:   10,
 		FollowerCount: 5,
 		IsFollow:      true,
 	},
+}
+
+// TODO token -> user id
+var usersLoginInfo = map[string]int64{
+	"zhanglei---douyin": 1,
 }
 
 var userIdSequence = int64(1)
@@ -32,61 +51,202 @@ type UserResponse struct {
 	User User `json:"user"`
 }
 
-func Register(c *gin.Context) {
-	username := c.Query("username")
-	password := c.Query("password")
+/**
+ * @description: 创建用户 token
+ * @param {string} username 用户名
+ * @param {string} password 用户密码
+ * @return {string} 生成的 token
+ */
+func createToken(username string, password string) string {
+	// TODO 加密
+	return username +"---"+ password
+}
 
-	token := username + password
+func decodeToken(token string) ([]string, error) {
+	// TODO 解密
+	res:=strings.Split(token, "---")
+	return res, nil
+}
 
-	if _, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: Response{StatusCode: 1, StatusMsg: "User already exist"},
-		})
-	} else {
-		atomic.AddInt64(&userIdSequence, 1)
-		newUser := User{
-			Id:   userIdSequence,
-			Name: username,
+/**
+ * @description: 计算用户id，改变内存中的id，返回计算后的id
+ * @param {*int64} id 当前id值
+ * @return {*} int64 计算后的id
+ */
+func calcUserId(id *int64) int64 {
+	// TODO 计算id
+	return atomic.AddInt64(id, 1)
+}
+
+/**
+ * @description: 获取用户信息
+ * @param {int64} id	用户id
+ * @return {*}	用户信息
+ */
+func getUserInfo(id int64) User {
+	return usersDate[id]
+}
+
+/**
+ * @description: 创建新用户，并存储
+ * @param {int64} id 用户id
+ * @param {string} name 用户名
+ * @param {string} password 用户密码
+ */
+func newUser(id int64, name string, password string) {
+	usersDate[id] = User{
+		Id:   id,
+		Name: name,
+	}
+	usersLoginInfo[name+password] = id
+}
+
+func searchUser(username string) (User, bool) {
+	for _, user := range usersDate {
+		if user.Name == username {
+			return user, true
 		}
-		usersLoginInfo[token] = newUser
+	}
+	return User{}, false
+}
+
+// 验证密码
+func verifyPassword(input string, password string) bool {
+	return input == password
+}
+
+/**
+ * @description: 验证用户登录中间件
+ * @param {string} token 请求参数
+ */
+func AuthMiddleware(c *gin.Context) {
+	// 获取token
+	token := c.Query("token")
+	// 验证token
+	if _, exist := usersLoginInfo[token]; exist {
+		c.Next()
+	} else {
 		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: Response{StatusCode: 0},
-			UserId:   userIdSequence,
-			Token:    username + password,
+			Response: Response{StatusCode: Fail, StatusMsg: NotLogin},
 		})
+		c.Abort()
 	}
 }
 
-func Login(c *gin.Context) {
+/**
+ * @description: 用户登录处理函数，返回用户登录信息
+ */
+func Register(c *gin.Context) {
+	// 1. http请求中获取用户信息
 	username := c.Query("username")
 	password := c.Query("password")
 
-	token := username + password
-
-	if user, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: Response{StatusCode: 0},
-			UserId:   user.Id,
-			Token:    token,
-		})
-	} else {
-		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
-		})
+	// 2. 查询用户是否存在，并返回用户信息
+	user, exist := searchUser(username)
+	// 3.1 存在则验证密码
+	if exist {
+		user.FollowCount = 0
+		// verifyPassword(password, user.Password)
 	}
+	// 3.2 不存在则创建用户，并返回用户信息
+	id := calcUserId(&userIdSequence)
+	newUser(id, username, password)
+	token := createToken(username, password)
+	c.JSON(http.StatusOK, UserLoginResponse{
+		Response: Response{StatusCode: Success, StatusMsg: SignUpOk},
+		UserId:   id,
+		Token:    token,
+	})
+
+	// token := createToken(username, password)
+
+	// if _, exist := usersLoginInfo[token]; exist {
+	// 	c.JSON(http.StatusOK, UserLoginResponse{
+	// 		Response: Response{StatusCode: Fail, StatusMsg: Existed},
+	// 	})
+	// } else {
+	// 	// newUser := User{
+	// 	// 	Id:   calcUserId(&userIdSequence),
+	// 	// 	Name: username,
+	// 	// }
+	// 	newUser(calcUserId(&userIdSequence), username, password)
+
+	// 	c.JSON(http.StatusOK, UserLoginResponse{
+	// 		Response: Response{StatusCode: Success},
+	// 		UserId:   userIdSequence,
+	// 		Token:    token,
+	// 	})
+	// }
 }
 
-func UserInfo(c *gin.Context) {
-	token := c.Query("token")
+/**
+ * @description: 用户登录处理函数，返回用户登录信息
+ */
+func Login(c *gin.Context) {
+	// 1. http请求中获取用户信息
+	username := c.Query("username")
+	password := c.Query("password")
 
-	if user, exist := usersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, UserResponse{
-			Response: Response{StatusCode: 0},
-			User:     user,
+	// 2. 查询用户是否存在，并返回用户信息
+	user, exist := searchUser(username)
+	if !exist {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: Response{StatusCode: Fail, StatusMsg: NotExisted},
 		})
-	} else {
-		c.JSON(http.StatusOK, UserResponse{
-			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
-		})
+		return
 	}
+	// 3. 验证密码
+	// if !verifyPassword(password, user.Password) {
+	// 	c.JSON(http.StatusOK, UserLoginResponse{
+	// 		Response: Response{StatusCode: Fail, StatusMsg: WrongPassword},
+	// 	})
+	// 	return
+	// }
+
+	// 4. 生成token，存储在服务器
+	token := createToken(username, password)
+	usersLoginInfo[token] = user.Id // 服务器存储token
+
+	// 5. 返回用户信息和token
+	c.JSON(http.StatusOK, UserLoginResponse{
+		Response: Response{StatusCode: Success},
+		UserId:   user.Id,
+		Token:    token,
+	})
+
+	// if user, exist := usersLoginInfo[token]; exist {
+	// 	c.JSON(http.StatusOK, UserLoginResponse{
+	// 		Response: Response{StatusCode: Success},
+	// 		UserId:   user.Id,
+	// 		Token:    token,
+	// 	})
+	// } else {
+	// 	c.JSON(http.StatusOK, UserLoginResponse{
+	// 		Response: Response{StatusCode: Fail, StatusMsg: NotExisted},
+	// 	})
+	// }
+}
+
+/**
+ * @description: 获取用户信息处理函数，返回用户信息
+ */
+func UserInfo(c *gin.Context) {
+
+	user_id, _ := strconv.ParseInt(c.Query("user_id"), 10, 64)
+
+	user := getUserInfo(user_id)
+	c.JSON(http.StatusOK, UserResponse{
+		Response: Response{StatusCode: Success},
+		User:     user,
+	})
+	// if user, exist := usersDate[user_id.(int64)]; exist {
+	// 	c.JSON(http.StatusOK, UserResponse{
+	// 		Response: Response{StatusCode: Success},
+	// 		User:     user,
+	// 	})
+	// } else {
+	// 	c.JSON(http.StatusOK, UserResponse{
+	// 		Response: Response{StatusCode: Fail, StatusMsg: NotExisted},
+	// 	})
+	// }
 }
