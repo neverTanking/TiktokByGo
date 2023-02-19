2c6bf7c63736f2d42370d9403085ca90950da006
xjx <2869418079@qq.com>
2023-02-19 20:20:37 +0800

    pull main

--- router/router.go
+++ router/router.go
@@ -1,12 +1,9 @@
 package router
 
 import (
+	"github.com/RaymondCode/simple-demo/controller"
 	"github.com/gin-gonic/gin"
-<<<<<<< HEAD
-=======
-	"github.com/neverTanking/TiktokByGo/controller"
 	"github.com/neverTanking/TiktokByGo/controller/video"
->>>>>>> like
 	"github.com/neverTanking/TiktokByGo/db"
 	"github.com/neverTanking/TiktokByGo/middleware/JWT"
 )
@@ -20,16 +17,15 @@ func InitRouter(r *gin.Engine) *gin.Engine {
 
 	// basic apis
 	apiRouter.GET("/feed/", controller.Feed)
-	//apiRouter.GET("/user/", controller.UserInfo)
-	//apiRouter.POST("/user/register/", controller.Register)
-	//apiRouter.POST("/user/login/", controller.Login)
-	//apiRouter.POST("/publish/action/", controller.Publish)
-	//apiRouter.GET("/publish/list/", controller.PublishList)
+	apiRouter.GET("/user/", controller.UserInfo)
+	apiRouter.POST("/user/register/", controller.Register)
+	apiRouter.POST("/user/login/", controller.Login)
+	apiRouter.POST("/publish/action/", controller.Publish)
+	apiRouter.GET("/publish/list/", controller.PublishList)
 
 	// extra apis - I
-<<<<<<< HEAD
-	apiRouter.POST("/favorite/action/", controller.FavoriteAction)
-	//apiRouter.GET("/favorite/list/", video.FavoriteList)
+	apiRouter.POST("/favorite/action/", JWT.JWTMiddleware(), video.LikeActionController)
+	apiRouter.GET("/favorite/list/", controller.FavoriteList)
 	apiRouter.POST("/comment/action/", controller.CommentAction)
 	apiRouter.GET("/comment/list/", controller.CommentList)
 
@@ -40,20 +36,5 @@ func InitRouter(r *gin.Engine) *gin.Engine {
 	apiRouter.GET("/relation/friend/list/", controller.FriendList)
 	apiRouter.GET("/message/chat/", controller.MessageChat)
 	apiRouter.POST("/message/action/", controller.MessageAction)
-=======
-	apiRouter.POST("/favorite/action/", JWT.JWTMiddleware(), video.LikeActionController)
-	//apiRouter.GET("/favorite/list/", controller.FavoriteList)
-	//
-	//apiRouter.POST("/comment/action/", controller.CommentAction)
-	//apiRouter.GET("/comment/list/", controller.CommentList)
-	//
-	//// extra apis - II
-	//apiRouter.POST("/relation/action/", controller.RelationAction)
-	//apiRouter.GET("/relation/follow/list/", controller.FollowList)
-	//apiRouter.GET("/relation/follower/list/", controller.FollowerList)
-	//apiRouter.GET("/relation/friend/list/", controller.FriendList)
-	//apiRouter.GET("/message/chat/", controller.MessageChat)
-	//apiRouter.POST("/message/action/", controller.MessageAction)
->>>>>>> like
 	return r
 }
