0702cf92886a45c22e0bb8657fc2c5401b582677
xjx <2869418079@qq.com>
2023-02-19 21:18:34 +0800

    sync github

--- router/router.go
+++ router/router.go
@@ -17,24 +17,24 @@ func InitRouter(r *gin.Engine) *gin.Engine {
 
 	// basic apis
 	apiRouter.GET("/feed/", controller.Feed)
-	apiRouter.GET("/user/", controller.UserInfo)
-	apiRouter.POST("/user/register/", controller.Register)
-	apiRouter.POST("/user/login/", controller.Login)
-	apiRouter.POST("/publish/action/", controller.Publish)
-	apiRouter.GET("/publish/list/", controller.PublishList)
+	//apiRouter.GET("/user/", controller.UserInfo)
+	//apiRouter.POST("/user/register/", controller.Register)
+	//apiRouter.POST("/user/login/", controller.Login)
+	//apiRouter.POST("/publish/action/", controller.Publish)
+	//apiRouter.GET("/publish/list/", controller.PublishList)
 
 	// extra apis - I
 	apiRouter.POST("/favorite/action/", JWT.JWTMiddleware(), video.LikeActionController)
-	apiRouter.GET("/favorite/list/", controller.FavoriteList)
-	apiRouter.POST("/comment/action/", controller.CommentAction)
-	apiRouter.GET("/comment/list/", controller.CommentList)
-
-	// extra apis - II
-	apiRouter.POST("/relation/action/", controller.RelationAction)
-	apiRouter.GET("/relation/follow/list/", controller.FollowList)
-	apiRouter.GET("/relation/follower/list/", controller.FollowerList)
-	apiRouter.GET("/relation/friend/list/", controller.FriendList)
-	apiRouter.GET("/message/chat/", controller.MessageChat)
-	apiRouter.POST("/message/action/", controller.MessageAction)
+	//apiRouter.GET("/favorite/list/", controller.FavoriteList)
+	//apiRouter.POST("/comment/action/", controller.CommentAction)
+	//apiRouter.GET("/comment/list/", controller.CommentList)
+	//
+	//// extra apis - II
+	//apiRouter.POST("/relation/action/", controller.RelationAction)
+	//apiRouter.GET("/relation/follow/list/", controller.FollowList)
+	//apiRouter.GET("/relation/follower/list/", controller.FollowerList)
+	//apiRouter.GET("/relation/friend/list/", controller.FriendList)
+	//apiRouter.GET("/message/chat/", controller.MessageChat)
+	//apiRouter.POST("/message/action/", controller.MessageAction)
 	return r
 }
