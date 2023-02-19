25f91ec2cfb481a8604979c81dde4772d5a2abff
zero1 <754483458@qq.com>
2023-02-19 20:12:05 +0800

    router_midware

--- router/router.go
+++ router/router.go
@@ -1,9 +1,10 @@
 package router
 
 import (
+	"JWT"
+
 	"github.com/RaymondCode/simple-demo/controller"
 	"github.com/gin-gonic/gin"
-	"github.com/neverTanking/TiktokByGo/controller/video"
 	"github.com/neverTanking/TiktokByGo/db"
 )
 
@@ -15,25 +16,25 @@ func InitRouter(r *gin.Engine) *gin.Engine {
 	apiRouter := r.Group("/douyin")
 
 	// basic apis
-	apiRouter.GET("/feed/", controller.Feed)
-	apiRouter.GET("/user/", controller.UserInfo)
-	apiRouter.POST("/user/register/", controller.Register)
-	apiRouter.POST("/user/login/", controller.Login)
-	apiRouter.POST("/publish/action/", controller.Publish)
-	apiRouter.GET("/publish/list/", controller.PublishList)
+	apiRouter.GET("/feed/", JWT.WTMiddleware(), controller.Feed)
+	apiRouter.GET("/user/", JWT.WTMiddleware(), controller.UserInfo)
+	apiRouter.POST("/user/register/", controller.Register) //
+	apiRouter.POST("/user/login/", controller.Login)       //
+	apiRouter.POST("/publish/action/", JWT.WTMiddleware(), controller.Publish)
+	apiRouter.GET("/publish/list/", JWT.WTMiddleware(), controller.PublishList)
 
 	// extra apis - I
-	apiRouter.POST("/favorite/action/", controller.FavoriteAction)
-	apiRouter.GET("/favorite/list/", video.FavoriteList)
-	apiRouter.POST("/comment/action/", controller.CommentAction)
-	apiRouter.GET("/comment/list/", controller.CommentList)
+	apiRouter.POST("/favorite/action/", JWT.WTMiddleware(), controller.FavoriteAction)
+	apiRouter.GET("/favorite/list/", JWT.WTMiddleware(), controller.FavoriteList)
+	apiRouter.POST("/comment/action/", JWT.WTMiddleware(), controller.CommentAction)
+	apiRouter.GET("/comment/list/", JWT.WTMiddleware(), controller.CommentList)
 
 	// extra apis - II
-	apiRouter.POST("/relation/action/", controller.RelationAction)
-	apiRouter.GET("/relation/follow/list/", controller.FollowList)
-	apiRouter.GET("/relation/follower/list/", controller.FollowerList)
-	apiRouter.GET("/relation/friend/list/", controller.FriendList)
-	apiRouter.GET("/message/chat/", controller.MessageChat)
-	apiRouter.POST("/message/action/", controller.MessageAction)
+	// apiRouter.POST("/relation/action/", controller.RelationAction)
+	// apiRouter.GET("/relation/follow/list/", controller.FollowList)
+	// apiRouter.GET("/relation/follower/list/", controller.FollowerList)
+	// apiRouter.GET("/relation/friend/list/", controller.FriendList)
+	// apiRouter.GET("/message/chat/", controller.MessageChat)
+	// apiRouter.POST("/message/action/", controller.MessageAction)
 	return r
 }
