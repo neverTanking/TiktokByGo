14bff2825191e9d2328baaac9f00a8b30a9adda2
ratio <ratio_dd@163.com>
2023-02-20 21:34:19 +0800

    🚧 revert controller/user.go to 2 commit ahead version

--- controller/user.go
+++ controller/user.go
@@ -1,15 +1,12 @@
-package controller
+package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/neverTanking/TiktokByGo/controller"
 	"github.com/neverTanking/TiktokByGo/model"
 	"net/http"
+	"strconv"
 )
-
-type User struct {
-	model.User
-	IsFollow bool `json:"is_follow"` // true-已关注，false-未关注
-}
 
 // token -> user_id
 var UsersLoginInfo = map[string]int64{
@@ -27,16 +24,31 @@ type UserResponse struct {
 	User model.User `json:"user"`
 }
 
+// Can use middleware JWT
+///**
+// * @description: 创建用户 token
+// * @param {string} username 用户名
+// * @param {string} password 用户密码
+// * @return {string} 生成的 token
+// */
+//func createToken(username string, password string) string {
+//	// TODO 加密
+//	return strings.Join([]string{username, password}, "---")
+//}
+//
+//func decodeToken(token string) ([]string, error) {
+//	// TODO 解密
+//	res := strings.Split(token, "---")
+//	return res, nil
+//}
+
 /**
  * @description: 获取用户信息
  * @param {int64} id	用户id
  * @return {*}	用户信息
  */
-func getUserInfo(id uint) model.User {
-	user, ok := model.SearchUserByID(id)
-	if ok != true {
-
-	}
+func getUserInfo(id int64) model.User {
+	user := model.SearchUser(id)
 	return user
 }
 
@@ -46,9 +58,17 @@ func getUserInfo(id uint) model.User {
  * @param {string} name 用户名
  * @param {string} password 用户密码
  */
-//func newUser(name string, password string) int64 {
-//	//return model.CreatUser(name, password)
-//}
+func newUser(name string, password string) int64 {
+	return model.CreatUser(name, password)
+}
+
+func SearchUserByName(username string) (model.User, bool) {
+	return model.User{}, false
+}
+
+func SearchUserById(userID uint) (model.User, bool) {
+	return model.User{}, false
+}
 
 // 验证密码
 func verifyPassword(input string, password string) bool {
@@ -67,7 +87,7 @@ func AuthMiddleware(c *gin.Context) {
 		c.Next()
 	} else {
 		c.JSON(http.StatusOK, UserLoginResponse{
-			Response: model.Response{StatusCode: Fail, StatusMsg: NotLogin},
+			Response: model.Response{StatusCode: controller.Fail, StatusMsg: controller.NotLogin},
 		})
 		c.Abort()
 	}
@@ -76,116 +96,116 @@ func AuthMiddleware(c *gin.Context) {
 /**
  * @description: 用户登录处理函数，返回用户登录信息
  */
-//func Register(c *gin.Context) {
-//	// 1. http请求中获取用户信息
-//	username := c.Query("username")
-//	password := c.Query("password")
-//
-//	// 2. 查询用户是否存在，并返回用户信息
-//	user, exist := SearchUserByName(username)
-//	// 3.1 存在则验证密码
-//	if exist {
-//		user.FollowCount = 0
-//		// verifyPassword(password, user.Password)
-//	}
-//	// 3.2 不存在则创建用户，并返回用户信息
-//	id := model.CreatUser(username, password)
-//	token := createToken(username, password)
-//	c.JSON(http.StatusOK, UserLoginResponse{
-//		Response: model.Response{StatusCode: Success, StatusMsg: SignUpOk},
-//		UserId:   id,
-//		Token:    token,
-//	})
-//
-//	// token := createToken(username, password)
-//
-//	// if _, exist := UsersLoginInfo[token]; exist {
-//	// 	c.JSON(http.StatusOK, UserLoginResponse{
-//	// 		Response: Response{StatusCode: Fail, StatusMsg: Existed},
-//	// 	})
-//	// } else {
-//	// 	// newUser := User{
-//	// 	// 	Id:   calcUserId(&userIdSequence),
-//	// 	// 	Name: username,
-//	// 	// }
-//	// 	newUser(calcUserId(&userIdSequence), username, password)
-//
-//	// 	c.JSON(http.StatusOK, UserLoginResponse{
-//	// 		Response: Response{StatusCode: Success},
-//	// 		UserId:   userIdSequence,
-//	// 		Token:    token,
-//	// 	})
-//	// }
-//}
+func Register(c *gin.Context) {
+	// 1. http请求中获取用户信息
+	username := c.Query("username")
+	password := c.Query("password")
+
+	// 2. 查询用户是否存在，并返回用户信息
+	user, exist := searchUser(username)
+	// 3.1 存在则验证密码
+	if exist {
+		user.FollowCount = 0
+		// verifyPassword(password, user.Password)
+	}
+	// 3.2 不存在则创建用户，并返回用户信息
+	id := model.CreatUser(username, password)
+	token := createToken(username, password)
+	c.JSON(http.StatusOK, UserLoginResponse{
+		Response: model.Response{StatusCode: controller.Success, StatusMsg: controller.SignUpOk},
+		UserId:   id,
+		Token:    token,
+	})
+
+	// token := createToken(username, password)
+
+	// if _, exist := UsersLoginInfo[token]; exist {
+	// 	c.JSON(http.StatusOK, UserLoginResponse{
+	// 		Response: Response{StatusCode: Fail, StatusMsg: Existed},
+	// 	})
+	// } else {
+	// 	// newUser := User{
+	// 	// 	Id:   calcUserId(&userIdSequence),
+	// 	// 	Name: username,
+	// 	// }
+	// 	newUser(calcUserId(&userIdSequence), username, password)
+
+	// 	c.JSON(http.StatusOK, UserLoginResponse{
+	// 		Response: Response{StatusCode: Success},
+	// 		UserId:   userIdSequence,
+	// 		Token:    token,
+	// 	})
+	// }
+}
 
 /**
  * @description: 用户登录处理函数，返回用户登录信息
  */
-//func Login(c *gin.Context) {
-//	// 1. http请求中获取用户信息
-//	username := c.Query("username")
-//	password := c.Query("password")
-//
-//	// 2. 查询用户是否存在，并返回用户信息
-//	user, exist := model.SearchUserByName(username)
-//	if !exist {
-//		c.JSON(http.StatusOK, UserLoginResponse{
-//			Response: model.Response{StatusCode: Fail, StatusMsg: NotExisted},
-//		})
-//		return
-//	}
-//	// 3. 验证密码
-//	// if !verifyPassword(password, user.Password) {
-//	// 	c.JSON(http.StatusOK, UserLoginResponse{
-//	// 		Response: Response{StatusCode: Fail, StatusMsg: WrongPassword},
-//	// 	})
-//	// 	return
-//	// }
-//
-//	// 4. 生成token，存储在服务器
-//	//token := createToken(username, password)
-//	//UsersLoginInfo[token] = user.Id // 服务器存储token
-//
-//	// 5. 返回用户信息和token
-//	//c.JSON(http.StatusOK, UserLoginResponse{
-//	//	Response: model.Response{StatusCode: Success},
-//	//	UserId:   user.Id,
-//	//	Token:    token,
-//	//})
-//
-//	// if user, exist := UsersLoginInfo[token]; exist {
-//	// 	c.JSON(http.StatusOK, UserLoginResponse{
-//	// 		Response: Response{StatusCode: Success},
-//	// 		UserId:   user.Id,
-//	// 		Token:    token,
-//	// 	})
-//	// } else {
-//	// 	c.JSON(http.StatusOK, UserLoginResponse{
-//	// 		Response: Response{StatusCode: Fail, StatusMsg: NotExisted},
-//	// 	})
-//	// }
-//}
+func Login(c *gin.Context) {
+	// 1. http请求中获取用户信息
+	username := c.Query("username")
+	password := c.Query("password")
+
+	// 2. 查询用户是否存在，并返回用户信息
+	user, exist := searchUser(username)
+	if !exist {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: model.Response{StatusCode: controller.Fail, StatusMsg: controller.NotExisted},
+		})
+		return
+	}
+	// 3. 验证密码
+	// if !verifyPassword(password, user.Password) {
+	// 	c.JSON(http.StatusOK, UserLoginResponse{
+	// 		Response: Response{StatusCode: Fail, StatusMsg: WrongPassword},
+	// 	})
+	// 	return
+	// }
+
+	// 4. 生成token，存储在服务器
+	token := createToken(username, password)
+	UsersLoginInfo[token] = user.Id // 服务器存储token
+
+	// 5. 返回用户信息和token
+	c.JSON(http.StatusOK, UserLoginResponse{
+		Response: model.Response{StatusCode: controller.Success},
+		UserId:   user.Id,
+		Token:    token,
+	})
+
+	// if user, exist := UsersLoginInfo[token]; exist {
+	// 	c.JSON(http.StatusOK, UserLoginResponse{
+	// 		Response: Response{StatusCode: Success},
+	// 		UserId:   user.Id,
+	// 		Token:    token,
+	// 	})
+	// } else {
+	// 	c.JSON(http.StatusOK, UserLoginResponse{
+	// 		Response: Response{StatusCode: Fail, StatusMsg: NotExisted},
+	// 	})
+	// }
+}
 
 /**
  * @description: 获取用户信息处理函数，返回用户信息
  */
-//func UserInfo(c *gin.Context) {
-//
-//	user_id, _ := strconv.ParseInt(c.Query("user_id"), 10, 64)
-//
-//	user := getUserInfo(user_id)
-//	c.JSON(http.StatusOK, UserResponse{
-//		Response: model.Response{StatusCode: Success},
-//		User:     user,
-//	})
-//	// if user, exist := usersDate[user_id.(int64)]; exist {
-//	// 	c.JSON(http.StatusOK, UserResponse{
-//	// 		Response: Response{StatusCode: Success},
-//	// 		User:     user,
-//	// 	})
-//	// } else {
-//	// 	c.JSON(http.StatusOK, UserResponse{
-//	// 		Response: Response{StatusCode: Fail, StatusMsg: NotExisted},
-//	// 	})
-//	// }
-//}
+func UserInfo(c *gin.Context) {
+
+	user_id, _ := strconv.ParseInt(c.Query("user_id"), 10, 64)
+
+	user := getUserInfo(user_id)
+	c.JSON(http.StatusOK, UserResponse{
+		Response: model.Response{StatusCode: controller.Success},
+		User:     user,
+	})
+	// if user, exist := usersDate[user_id.(int64)]; exist {
+	// 	c.JSON(http.StatusOK, UserResponse{
+	// 		Response: Response{StatusCode: Success},
+	// 		User:     user,
+	// 	})
+	// } else {
+	// 	c.JSON(http.StatusOK, UserResponse{
+	// 		Response: Response{StatusCode: Fail, StatusMsg: NotExisted},
+	// 	})
+	// }
+}
