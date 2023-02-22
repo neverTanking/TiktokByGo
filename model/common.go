0d38f5089766ca4173aa406717755303a7081455
ratio <ratio_dd@163.com>
2023-02-22 17:08:04 +0800

    🐛 Repair Video struct: add author

--- model/common.go
+++ model/common.go
@@ -19,7 +19,7 @@ type User struct {
 // 单个视频
 type Video struct {
 	db.Video
-	User                //author信息都在这里面
+	Author        User  `json:"author"` //author信息都在这里面
 	FavoriteCount int64 `json:"favorite_count"`
 	CommentCount  int64 `json:"comment_count"`
 	IsFavorite    bool  `json:"is_favorite"`
